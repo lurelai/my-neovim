989d3cc5bb18b57b58e85fc4a334a6346a5fef35
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-10 17:37:42 -0300

    finish maps

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,6 +1,9 @@
 return function()
-	vim.keymap.set('n', '<C-q>', ':q<CR>')
-	vim.keymap.set('n', '<C-s>', ':w<CR>')
-	vim.keymap.set('n', ';', ':')
+	vim.keymap.set('n', '<C-q>', ':q<CR>') 			-- Easy quit
+	vim.keymap.set('n', '<C-s>', ':w<CR>')			-- Easy save
+	vim.keymap.set('n', ';', ':')					-- Ease way to enter in the type mode
+
+	vim.keymap.set('i', '<C-q>', '<ESC>:q<CR>')		-- easy way to quit in the insert mode
+	vim.keymap.set('i', '<C-s>', '<ESC>:w<CR>a')	-- easy way to save in the insert mode
 end
 
