6a3d5bd0848a6ef2f4acb5ea8349a4eb3b0d12dd
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-10 17:32:04 -0300

    adding remaps

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = ","
 
 require("options")()
+require("remaps")()
 
---
+++ lua/remaps.lua
@@ -0,0 +1,6 @@
+return function()
+	vim.keymap.set('n', '<C-q>', ':q<CR>')
+	vim.keymap.set('n', '<C-s>', ':w<CR>')
+	vim.keymap.set('n', ';', ':')
+end
+
