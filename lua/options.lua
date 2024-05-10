d4ad45da239b67b89e0bdcca299c24a78e0566d3
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-10 17:01:00 -0300

    finish options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,27 @@
 return function()
-	vim.opt.rnu=true
+	vim.opt.rnu 					= true 				-- Relative numbers option
+	vim.opt.showtabline 			= 2					-- Allow the tabline on the top of the screen
+	vim.opt.tabstop 				= 4					-- Set the tabstop as 4 (default tab of files)
+	vim.opt.shiftwidth				= 4					-- Set the default tab as 4 (the tab typed)
 
-	vim.opt.filetype="on"
+	vim.opt.guicursor				= "n-v-c-i:block"	-- Set the insert-cursor as block
+	vim.opt.hidden 					= true				-- Hide the current buffer when a new file is created
+
+	vim.opt.incsearch				= true				-- Incremental search
+	vim.opt.ignorecase				= true				-- Ignore case in search
+	vim.opt.smartcase				= true				-- Consider case if there's a uppercase character
+
+	vim.opt.signcolumn				= "yes" 			-- Set a padding on the left
+	vim.opt.colorcolumn				= "120" 			-- Create a column on the right
+
+	vim.opt.laststatus				= 3					-- Set the status line always the same
+	vim.opt.cmdheight				= 2					-- More space to the output messages
+	vim.opt.encoding				= "utf-8"			-- Allows utf-8
+		
+	vim.opt.backup					= false 			-- Don't create backup
+	vim.opt.writebackup				= false				-- Don't write any backup
+
+	vim.opt.autoread				= true				-- Show changes at the file on the real time
+	vim.opt.mouse					= "a"
 end
 
