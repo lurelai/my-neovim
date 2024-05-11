f644e0c433c3265cc5991a8cf8674d510168c61a
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-10 23:49:36 -0300

    finish the 'link.sh'

--- plugins/link.sh
+++ plugins/link.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-ln -fsr pack $HOME/.local/share/nvim/site/pack
-unlink pack/pack
+NEOVIMLINK="$HOME/.local/share/nvim/site/pack"
+CURRENTDIR="$(dirname $0)"
+
+
+ln -fsr ${CURRENTDIR}/pack $NEOVIMLINK
+unlink ${CURRENTDIR}/pack/pack
 
 exit 0
 
